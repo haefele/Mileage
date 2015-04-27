4efedb5bed63b71db590c933d89f64999b9b5b5b
Daniel Häfele <haefele@xemio.net>
2015-04-27 21:44:56 +0200

    Created the index DocumentsWithTags to retrieve a top list with the most used tags.
    Working on #26.

---
+++ src/03 Server/Mileage.Server.Infrastructure/Raven/Indexes/DocumentsWithTags.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Mileage.Shared.Entities.Search;
+using Raven.Client.Indexes;
+using Raven.Database.FileSystem.Storage.Esent.Schema.Updates;
+
+namespace Mileage.Server.Infrastructure.Raven.Indexes
+{
+    public class DocumentsWithTags : AbstractMultiMapIndexCreationTask<DocumentsWithTags.Result>
+    {
+        public class Result
+        {
+            public string Tag { get; set; }
+            public int Count { get; set; }
+        }
+
+        public DocumentsWithTags()
+        {
+            this.AddMapForAll<ITaggable>(documents => 
+                from document in documents
+                from tag in document.Tags
+                select new
+                {
+                    Tag = tag,
+                    Count = 1
+                });
+
+            this.Reduce = results =>
+                from result in results
+                group result by result.Tag into g
+                select new
+                {
+                    Tag = g.Key,
+                    Count = g.Sum(f => f.Count)
+                };
+        }
+
+        public override string IndexName
+        {
+            get { return "Documents/WithTags"; }
+        }
+    }
+}
